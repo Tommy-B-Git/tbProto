6e78b0e6fc7551d5760acd0753db1dfbb2112c8b
Tommy Brown <tommy_brown@me.com>
2017-01-31 15:16:40 +0000

    update main Js

--- js/main.js
+++ js/main.js
@@ -4,23 +4,33 @@ $(function() {
 
     // Cache the Window object
     var $window = $(window);
-
     // Parallax Background
     $('section[data-type="background"]').each(function() {
         var $bgobj = $(this); // assigning the object
-
         $(window).scroll(function() {
-
             // Scroll the background at var speed
             // the yPos is a negative value because we're scrolling it UP!								
             var yPos = -($window.scrollTop() / $bgobj.data('speed'));
-
             // Put together our final background position
             var coords = '50% ' + yPos + 'px';
-
             // Move the background
             $bgobj.css({ backgroundPosition: coords });
-
         }); // end window scroll
     });
+});
+
+// S M O O T H   S C R O L L I N G
+
+$('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+            $('html,body').animate({
+                scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
+    }
 });
