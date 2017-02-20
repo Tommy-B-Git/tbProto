81f5f92f4596ca9574a61b1c1396f350e581dea5
Tommy Brown <tommy_brown@me.com>
2017-02-20 17:20:10 +0000

    no changes

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $(function() {
         var $bgobj = $(this); // assigning the object
         $(window).scroll(function() {
             // Scroll the background at var speed
-            // the yPos is a negative value because we're scrolling it UP!								
+            // the yPos is a negative value because we're scrolling it UP!                              
             var yPos = -($window.scrollTop() / $bgobj.data('speed'));
             // Put together our final background position
             var coords = '50% ' + yPos + 'px';
@@ -20,16 +20,16 @@ $(function() {
 // S M O O T H   S C R O L L I N G
 
 $('a[href*="#"]:not([href="#"])').click(function() {
-  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-    var target = $(this.hash);
-    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-    if (target.length) {
-      $('html, body').animate({
-        scrollTop: target.offset().top
-      }, 1000);
-      return false;
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
     }
-  }
 });
 
 
---
+++ js/slider.js
@@ -0,0 +1,50 @@
+$(document).ready(function($) {
+
+    $('#checkbox').change(function() {
+        setInterval(function() {
+            moveRight();
+        }, 3000);
+    });
+
+    var slideCount = $('#slider ul li').length;
+    var slideWidth = $('#slider ul li').width();
+    var slideHeight = $('#slider ul li').height();
+    var sliderUlWidth = slideCount * slideWidth;
+
+    $('#slider').css({ width: slideWidth, height: slideHeight });
+
+    $('#slider ul').css({ width: sliderUlWidth, marginLeft: -slideWidth });
+
+    $('#slider ul li:last-child').prependTo('#slider ul');
+
+    function moveLeft() {
+        $('#slider ul').animate({
+            left: +slideWidth
+        }, 500, function() {
+            $('#slider ul li:last-child').prependTo('#slider ul');
+            $('#slider ul').css('left', '');
+        });
+    };
+
+    function moveRight() {
+        $('#slider ul').animate({
+            left: -slideWidth
+        }, 500, function() {
+            $('#slider ul li:first-child').appendTo('#slider ul');
+            $('#slider ul').css('left', '');
+        });
+    };
+
+    $('a.control_prev').click(function() {
+        moveLeft();
+        //To stop page jump on click
+        return false;
+    });
+
+    $('a.control_next').click(function() {
+        moveRight();
+        //To stop page jump on click
+        return false;
+    });
+
+});
